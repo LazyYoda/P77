6888de366e4e6cf7f377f7573887035e6f43dd78
LazyYoda <91386694+LazyYoda@users.noreply.github.com>
2022-03-26 13:00:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname = "Aritra";
+var lastname = "Barua";
+console.log(firstname);
+console.log(lastname);
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+var one = "1";
+var two = "2";
+console.log(one);
+console.log(two);
+var twelve = one.concat(two);
+console.log(twelve);
